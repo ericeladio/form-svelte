56c0bf0711c78421549693c2e8706669de22336b
Eric <diego123@Eric.local>
2024-03-09 22:53:36 -0600

    add 3 step from form

---
+++ src/components/CustomCheckBox.svelte
@@ -0,0 +1,40 @@
+<script>
+    export let checks;
+    let activeName = [];
+    const handler = (name) => {
+        if (activeName.includes(name)) {
+            activeName = activeName.filter((item) => item !== name);
+        } else {
+            activeName = [...activeName, name];
+        }
+    };
+</script>
+
+<div class="flex flex-col h-[400px] justify-evenly">
+    {#each checks as check}
+        <div
+            class={activeName.includes(check.name)
+                ? " h-[80px] flex items-center border-2 border-blue-950 rounded-md justify-around bg-LightBlue hover:cursor-pointer"
+                : " h-[80px] flex items-center border-2 border-blue-950 rounded-md justify-around  hover:bg-LightBlue hover:cursor-pointer"}
+            on:click={handler(check.name)}
+        >
+            <input
+                type="checkbox"
+                id={check.name}
+                name={check.name}
+                value={check.name}
+                checked={activeName.includes(check.name)}
+                class="h-5 w-5 mx-4 hover:cursor-pointer"
+            />
+            <label class="w-full hover:cursor-pointer" for="vehicle1">
+                <div class="flex justify-between items-center mr-4">
+                    <div>
+                        <h3 class="text-blue-950 font-bold">{check.name}</h3>
+                        <span class="">{check.description}</span>
+                    </div>
+                    <span class="text-blue-950">{check.price}</span>
+                </div>
+            </label>
+        </div>
+    {/each}
+</div>
--- src/info.js
+++ src/info.js
@@ -36,3 +36,9 @@ export const items = [
     { name: "Advanced", price: "$12/mo" },
     { name: "Pro", price: "$15/mo" },
 ];
+
+export const checks =[
+    { name: "Online service",description: "Access to multiplayer games", price: "+$1/mo" },
+    { name: "Larger storage",description: "Extra 1TB of cloud save", price: "+$2/mo" },
+    { name: "Customizable profile",description: "Custom theme on your profile", price: "+$2/mo" },
+]
